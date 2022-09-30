ca1ec94db01f8ce2b5118dfaf074037837191426
qwemnbpoizxc <114667248+qwemnbpoizxc@users.noreply.github.com>
2022-09-30 09:09:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -113,22 +113,6 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "data": {
             "date": {
                 "value": "{} {}".format(today, week),
-                "color": get_color()
-            },
-            "region": {
-                "value": region_name,
-                "color": get_color()
-            },
-            "weather": {
-                "value": weather,
-                "color": get_color()
-            },
-            "temp": {
-                "value": temp,
-                "color": get_color()
-            },
-            "wind_dir": {
-                "value": wind_dir,
                 "color": get_color()
             },
             "love_day": {
@@ -196,5 +180,5 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, note_ch, note_en)
     os.system("pause")
